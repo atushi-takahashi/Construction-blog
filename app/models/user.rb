fa0c15668642186f155b9270a188a7d404cf592a
EC2 Default User <ec2-user@ip-172-31-73-20.ec2.internal>
2020-11-20 13:41:00 +0000

    update[model=>association(user,post,question,category)]

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :name, length: { minimum: 1, maximum: 20 }, uniqueness: true
+  
+  has_many :posts, dependent: :destroy
+  has_many :questions, dependent: :destroy
 end
