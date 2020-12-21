55720fddffd41f37140a5c2167069c089a341350
EC2 Default User <ec2-user@ip-172-31-73-20.ec2.internal>
2020-12-21 13:42:19 +0000

    update[rspec(comments,likes)]

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
-  factory :comment do
-    
+  factory :post_comment, class: Comment do
+    message {"テストコメント"}
+    user_id { 1 }
+    post_id { 1 }
+    question_id { nil }
+  end
+  factory :question_comment, class: Comment do
+    message {"テストコメント"}
+    user_id { 1 }
+    post_id { nil }
+    question_id { 1 }
   end
 end
--- spec/factories/likes.rb
+++ spec/factories/likes.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
-  factory :like do
-    
+  factory :post_like, class: Like do
+    user_id { 1 }
+    post_id { 1 }
+    question_id { nil }
+  end
+  factory :question_like, class: Like do
+    user_id { 1 }
+    post_id { nil }
+    question_id { 1 }
   end
 end
